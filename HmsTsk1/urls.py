0f47a3658d23b8c6f18bf6424544c702215923ac
udaykumaraodh <89297495+udaykumaraodh@users.noreply.github.com>
2021-08-27 14:22:52 +0530

    Add files via upload

---
+++ HmsTsk1/urls.py
@@ -0,0 +1,39 @@
+"""HmsTsk1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path,include
+from HmsTsk1 import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('sales/',include('sales.urls')),
+    path('hospital/',include('hospital.urls')),
+    path('hadmin/',include('hmsadmin.urls')),
+    path('gettoken/',TokenObtainPairView.as_view(),name='gettoken'),
+    path('refresh/',TokenRefreshView.as_view(),name='refresh'),
+
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
